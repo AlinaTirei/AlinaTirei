208aa76a04072b89fb3bd79d493e158310bf2645
alinatirei9 <alinamaria9tirei@gmail.com>
2021-12-10 23:20:29 +0200

    Added homeworks

---
+++ homeworks/Tema 1- Civitas/temacivitas.js
@@ -0,0 +1,96 @@
+var securityCompany = {
+    name: "Security Company"
+    dispatchers: [{
+        firstName: "Ioana",
+        lastName: "Popescu",
+        age: 30,
+        rank: "Senior Dispatcher",
+        yearsinService: 6,
+        address: {
+            streetNumber: 2,
+            streetName: "Gheorghe Doja",
+            postcode: "200122",
+            county: "Dolj",
+            city: "Craiova",
+
+        }
+    }
+    
+      {
+        firstName: "Silviu",
+        lastName: "Grigorescu",
+        age: 25,
+        rank: "Junior Dispatcher",
+        yearsinService: 1,
+        address: {
+            streetNumber: 14,
+            streetName: "Calea Unirii",
+            postcode: "200769",
+            county: "Dolj",
+            city: "Segarcea",
+        }
+    }
+
+        {
+        firstName: "Matei",
+        lastName: "Teodoras",
+        age: 40,
+        rank: "Mid Dispatcher",
+        yearsinService: 3,
+        address: {
+            streetNumber: 1,
+            streetName: "Mihail Kogalniceanu",
+            postcode: "200182",
+            county: "Dolj",
+            city: "Craiova",
+
+        }
+
+    }
+    
+      
+}]
+
+    AuxiliarStaff: {
+        Admin = {
+    firstName: "Elena",
+    surname: "Avram",
+    phone: "0761XXXXXX",
+}
+
+Director = {
+    firstName: "Zoe",
+    surname: "Vancea",
+    phone: "0732974299",
+}
+
+Manager = {
+    firstName: "Grigore",
+    surname: "Andreescu",
+    phone: "0733890654",
+}
+
+    }
+
+cars = [{
+    make: "Dacia",
+    model: "Duster",
+    version: "1.5 Blue dCi 4WD",
+    year: 2019,
+    fulltype: "Diesel",
+    seats: "5",
+    colour: "white",
+    particle filter: "yes",
+    cilindrical capacity: "1 461 cm3",
+
+}
+
+{
+    make: "Dacia",
+    model: "Logan",
+    year: 2013,
+    fulltype: "Diesel",
+    seats: "5",
+    colour: "blue",
+
+}]
---
+++ homeworks/Tema 2- BMI/bmi.js
@@ -0,0 +1,26 @@
+
+var nutritioncenter = {
+        name: "Nutrition Center",
+        customers: [{
+                firstName: "John",
+                lastName: "Doe",
+                gender: "M",
+                bmi: 24,
+                height: "1.80cm",
+                weight: "78kg",
+                BodyType: "Normal Weight"
+        }
+
+        {
+                firstName: "Mary",
+                lastName: "Anne",
+                gender: "F",
+                bmi: 30,
+                height: "1.60cm",
+                weight: "77kg",
+                BodyType: "Obese"
+        }
+
+        ]
+
+
